9da3cfa97fd68abe1c2f5b1ec93580791507ba48
zCoder <40376705+zchambers3@users.noreply.github.com>
2019-01-20 20:47:34 -0700

    Create MemoryManagement.c

---
+++ MemoryManagement/MemoryManagement.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+int main() {
+srand(time(NULL));
+	int r;
+	r=rand()%4096;
+	printf(" \n%i",r);
+	printf(" \nbinary = 0x%08x", r);
+	printf(" \nhexadecimal = %04x",r);
+	printf(" \nNumber is Shifted By 16 Bits : %d\n",r << 16);
+return 0;
+}
